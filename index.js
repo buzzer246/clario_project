274fb366a327b362426cc6bfac0f1f2cb9ee6207
AVINASH REDDY K <48565975+buzzer246@users.noreply.github.com>
2022-12-07 20:46:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -29,18 +29,93 @@ $(document).ready(function () {
     });
 
 });
+$(document).ready(function () {
+    $("#flip5").click(function () {
+        $("#panel5").slideToggle("2000");
+
+    });
+
+});
 
 
 var loading = () => {
 
     var msg = "<h1> Please, wait Loading...</h1>";
     document.getElementById("container").innerHTML = msg;
-    
+
 }
+
+var register = () => {
+
+    
+
+    //alert("User Registered Sucessfully..!");
+
+    var username = document.getElementById("fname").value;
+    var password = document.getElementById("pass").value;
+    var mobile = document.getElementById("mobile").value;
+    var address = document.getElementById("address").value;
+
+    var formstatus = true;
+
+    if (fname == "") {
+
+        formstatus = false;
+        document.getElementById("fname").style.borderColor = "red";
+    }
+    else {
+
+        document.getElementById("fname").style.borderColor = "green";
+    }
+    if (password == "" || password.length < 6 || password != password) {
+
+        formstatus = false;
+        document.getElementById("pass").style.borderColor = "red";
+    }
+    else {
+
+        document.getElementById("pass").style.borderColor = "green";
+    }
+
+    if (mobile == "") {
+
+        formstatus = false;
+        document.getElementById("mobile").style.borderColor = "red";
+
+    } else {
+
+        document.getElementById("mobile").style.borderColor = "green";
+    }
+
+    if (address == "") {
+
+        formstatus = false;
+        document.getElementById("address").style.borderColor = "red";
+    } else {
+
+        document.getElementById("address").style.borderColor = "green";
+    }
+    if (formstatus == false) {
+        document.getElementById("errormsg").innerText = "User Not in Database";
+
+    }
+    else {
+
+        document.getElementById("errormsg").innerText = "User Sucessfully Registered";
+
+    }
+   
+
+}
+var clear = () => {
+
+    window.location.href = "http://127.0.0.1:5500/clario.html";
+}
+
 var save = () => {
     loading();
     var username = "Avinash Reddy K";
-    var url = "http://localhost:1234/userlist"+name;
+    var url = "http://localhost:1234/userlist" + name;
     fetch(url)
         .then(response => response.json())
         .then(userArray => {
@@ -50,7 +125,7 @@ var save = () => {
                 if (username == name) {
 
                     alert("Please SignUp ..!");
-   
+
                 } else {
 
                     alert("User is Logged-in Successfully");
@@ -61,63 +136,24 @@ var save = () => {
 
         })
 
-        var username = document.getElementById("fname").value;
-         var password = document.getElementById("pass").value;
-         
-
-        var formstatus = true;
-
-        if(fname=="") {
-
-            formstatus = false;
-            document.getElementById("fname").style.borderColor = "red";
-        }
-        else{
-
-            document.getElementById("fname").style.borderColor = "green";
-        }
-        if(password=="" || password.length<6 || password !=password) {
-
-            formstatus = false;
-            document.getElementById("pass").style.borderColor = "red";
-        }
-        else{
-
-            document.getElementById("pass").style.borderColor = "green";
-        }
-
-        
-        if(formstatus==false) {
-            document.getElementById("errormsg").innerText = "User Not in Database";
-
-        }
-        else{
-
-            document.getElementById("errormsg").innerText = "User Success, please wait..!"
-
-        }
-
 }
 
 let input = document.querySelector(".input");
-        let button = document.querySelector(".button");
-        
-        button.disabled = true; //setting button state to disabled
-        
-        input.addEventListener("change", stateHandle);
-        
-        function stateHandle() {
-            if (document.querySelector(".input").value === "") {
-                button.disabled = true; //button remains disabled
-            } else {
-                button.disabled = false; //button is enabled
-            }
-        }
-        
-var clear = () =>{
+let button = document.querySelector(".button");
 
-    window.location.href="http://127.0.0.1:5500/clario.html";
+button.disabled = true; //setting button state to disabled
+
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if (document.querySelector(".input").value === "") {
+        button.disabled = true; //button remains disabled
+    } else {
+        button.disabled = false; //button is enabled
+    }
 }
+
+
 
 
 
